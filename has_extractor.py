1f1e62cb6565753ed1a6986db2897b1012950c22
Simon Meoni <simonmeoni@aol.com>
2021-03-05 18:27:23 +0100

    first commit

---
+++ has_extractor.py
@@ -0,0 +1,25 @@
+import json
+import click
+from bs4 import BeautifulSoup
+
+
+def extract_content(content):
+    soup = BeautifulSoup(str(content), 'html.parser')
+    soup_find = soup.find("div", class_="wysiwyg basic")
+    if soup_find is not None:
+        txt = " ".join(div.text for div in soup_find.findChildren(recursive=True))
+        return txt
+    return None
+
+
+@click.command()
+@click.option('--input-path', required=True)
+@click.option('--output-path', required=True)
+def main(input_path, output_path):
+    data = {d['$']['url']: extract_content(d['field']) for d in json.load(open(input_path, "r"))}
+    data = {url: content for url, content in data.items() if content is not None}
+    json.dump(data, open(output_path, 'w'), indent=4, sort_keys=True)
+
+
+if __name__ == '__main__':
+    main()
