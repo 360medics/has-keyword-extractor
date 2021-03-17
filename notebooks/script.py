ac96126b76bc6085af8424a1b64aef83e340d7fa
Simon Meoni <simonmeoni@aol.com>
2021-03-17 15:32:24 +0100

    * reorganize project file structure

--- notebooks/script.py
+++ notebooks/script.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 
 
 def extract_content(content):
-    soup = BeautifulSoup(str(content), 'html.parser')
+    soup = BeautifulSoup(str(content), "html.parser")
     soup_find = soup.find("div", class_="wysiwyg basic")
     if soup_find is not None:
         txt = " ".join(div.text for div in soup_find.findChildren(recursive=True))
@@ -13,13 +13,16 @@ def extract_content(content):
 
 
 @click.command()
-@click.option('--input-path', required=True)
-@click.option('--output-path', required=True)
+@click.option("--input-path", required=True)
+@click.option("--output-path", required=True)
 def main(input_path, output_path):
-    data = {d['$']['url']: extract_content(d['field']) for d in json.load(open(input_path, "r"))}
+    data = {
+        d["$"]["url"]: extract_content(d["field"])
+        for d in json.load(open(input_path, "r"))
+    }
     data = {url: content for url, content in data.items() if content is not None}
-    json.dump(data, open(output_path, 'w'), indent=4, sort_keys=True)
+    json.dump(data, open(output_path, "w"), indent=4, sort_keys=True)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
