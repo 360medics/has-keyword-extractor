21d4c6189b54b88c80af5dfbb4d0f9574e437913
Simon Meoni <simonmeoni@aol.com>
2021-03-19 15:11:03 +0100

    multithread (#12)
    * I wrote pool thread inside st_process_multiple_doc method 
    * this close #11

--- has-keyword-extractor/_runner.py
+++ has-keyword-extractor/_runner.py
@@ -1,3 +1,5 @@
+from concurrent import futures
+from concurrent.futures.thread import ThreadPoolExecutor
 from typing import Dict
 
 from alive_progress import alive_bar
@@ -13,13 +15,20 @@ def st_process_doc(text: str, nlp, alpha: float, threshold: float):
 
 
 def st_process_multiple_doc(
-    documents: Dict[str, str], nlp, alpha: float, threshold: float
+    documents: Dict[str, str], nlp, alpha: float, threshold: float, workers=20
 ):
-    print('🔎 Begin keyword extraction : ')
+    print("🔎 Begin keyword extraction : ")
     result = {}
-    with alive_bar(len(documents)) as progress_bar:
-        for title, content in documents.items():
-            result[title] = st_process_doc(content, nlp, alpha, threshold)
-            progress_bar()
-    print('keyword extraction is ended')
+    with alive_bar(
+        len(documents), title="number of processed documents", length=100
+    ) as progress_bar:
+        with ThreadPoolExecutor(workers) as executor:
+            future_to_url = {
+                executor.submit(st_process_doc, content, nlp, alpha, threshold): title
+                for title, content in documents.items()
+            }
+            for future in futures.as_completed(future_to_url):
+                result[future_to_url[future]] = future.result()
+                progress_bar()
+    print("keyword extraction is ended")
     return result
