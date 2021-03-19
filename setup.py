6c3edab5aaaf6cc94212857c96afb7ea020b66e6
Simon Meoni <simonmeoni@aol.com>
2021-03-19 16:25:42 +0100

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ print(find_packages())
 setup(
     name="keyword-extraction",
     packages=find_packages(),
-    version="0.1.0",
+    version="0.2.0",
     description="the implementation  ths keyword extraction technique "
                 "described in the following paper : https://tinyurl.com/59b9ewb2",
     author="Simon Meoni",
