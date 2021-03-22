58939a151773520b329265a6f71b6fb1fa58560f
Simon Meoni <simonmeoni@aol.com>
2021-03-22 15:17:19 +0100

    * keep unlemmatized words * add nltk stopwords list * add a custom stopwords list * remove unnecessary newline when join on runner method

--- has_keyword_extractor/_preprocessing_text.py
+++ has_keyword_extractor/_preprocessing_text.py
@@ -1,16 +1,31 @@
 import re
+from nltk.corpus import stopwords
 
 
 def preprocessing_text(text: str, nlp, sep="[PUNCT]") -> list:
-    text = re.sub(r"\d+", "", text).lower()
     # tokenize text
     doc = nlp(text)
     # remove stopwords
     words_list = []
-
+    text = (
+        re.sub(r"\d+", "", text)
+        .lower()
+        .replace("\\n", "")
+        .replace("\\t", "")
+        .replace("\\xa", "")
+        .replace("\n", "")
+        .replace("'", "")
+        .strip()
+    )
+    stopwords_custom = stopwords.words("french") + [
+        "ci-dessous",
+        "ci-dessus",
+        "préalablement",
+        "précédemment",
+    ]
     for token in doc:
-        if token.is_punct or token.is_stop:
+        if token.is_punct or token.is_stop or token.text in stopwords_custom:
             words_list.append(sep)
         elif not token.is_space:
-            words_list.append(token.lemma_)
+            words_list.append(token.text)
     return words_list
